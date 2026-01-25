6db6717d08adce94b98a0187bf17d08fbfd84f52
breno-777 <breno.ba777@gmail.com>
2026-01-25 17:15:34 -0300

    System update, first update version implemented

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,9 +1,23 @@
 import { SelectFolderCard } from "../components/cards/select-folder-card"
 import { useSettings } from "../hooks/useSettings"
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+
 export const ScreenSettings = () => {
     const outputPaths = useSettings((state) => state.outputPaths)
     const selectOutputPath = useSettings((state) => state.selectOutputPath)
-    
+
+    const checkForUpdates = async () => {
+        const update = await check();
+        if (update) {
+            console.log('Update available:', update.version);
+            console.log('Notes:', update.body ?? 'No notes');
+            await update.downloadAndInstall();
+            await relaunch();
+        } else {
+            console.log('No update');
+        }
+    };
 
     return (
         <div>
@@ -35,6 +49,10 @@ export const ScreenSettings = () => {
                     onClick={() => selectOutputPath("videoConverter")}
                 />
             </div>
+            <button
+                style={{ borderRadius: 4, outline: "none", border: "none", padding: "4px 8px", backgroundColor: "#F59300", cursor: "pointer" }}
+                onClick={checkForUpdates}
+            >Check for updates</button>
         </div>
     )
 }
